1045a1da8b2a0738992c39aa8c5a5d03cd87ec18
codexpage <ywx@192.168.0.101>
2016-08-10 22:58:54 +0800

    new data collector and add database struct

---
+++ data/ontology-struct.sql
@@ -0,0 +1,80 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : ontology1
+ Source Server Type    : MySQL
+ Source Server Version : 50621
+ Source Host           : localhost
+ Source Database       : ontology
+
+ Target Server Type    : MySQL
+ Target Server Version : 50621
+ File Encoding         : utf-8
+
+ Date: 08/10/2016 22:57:31 PM
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `CO2sensor`
+-- ----------------------------
+DROP TABLE IF EXISTS `CO2sensor`;
+CREATE TABLE `CO2sensor` (
+  `id` int(11) NOT NULL,
+  `sensorid` int(11) DEFAULT NULL,
+  `CO2` double DEFAULT NULL,
+  `date` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+--  Table structure for `humiditysensor`
+-- ----------------------------
+DROP TABLE IF EXISTS `humiditysensor`;
+CREATE TABLE `humiditysensor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sensorid` int(11) DEFAULT NULL,
+  `humidity` double DEFAULT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+--  Table structure for `infraredsensor`
+-- ----------------------------
+DROP TABLE IF EXISTS `infraredsensor`;
+CREATE TABLE `infraredsensor` (
+  `id` int(11) NOT NULL,
+  `sensorid` int(11) DEFAULT NULL,
+  `infrared` double(11,0) DEFAULT NULL,
+  `date` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+--  Table structure for `luminancesensor`
+-- ----------------------------
+DROP TABLE IF EXISTS `luminancesensor`;
+CREATE TABLE `luminancesensor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sensorid` int(11) DEFAULT NULL,
+  `luminance` double DEFAULT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+--  Table structure for `temperaturesensor`
+-- ----------------------------
+DROP TABLE IF EXISTS `temperaturesensor`;
+CREATE TABLE `temperaturesensor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sensorid` int(11) DEFAULT NULL,
+  `temperature` double DEFAULT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=936 DEFAULT CHARSET=latin1;
+
+SET FOREIGN_KEY_CHECKS = 1;
---
+++ src/dataCollector/Collector.java
@@ -0,0 +1,105 @@
+package dataCollector;
+
+import java.math.BigDecimal;
+
+import message.AppMessage;
+import net.tinyos.message.Message;
+import net.tinyos.message.MessageListener;
+import net.tinyos.message.MoteIF;
+import net.tinyos.packet.BuildSource;
+import net.tinyos.util.PrintStreamMessenger;
+import tju.edu.model.sensor;
+import tju.edu.model.sensorDAO;
+import tju.edu.model.temperaturesensor;
+import tju.edu.model.temperaturesensorDAO;
+
+public class Collector implements MessageListener {
+	private MoteIF moteIF;
+
+	public Collector(String source) {
+		if (source != null) {
+			moteIF = new MoteIF(BuildSource.makePhoenix(source,
+					PrintStreamMessenger.err));
+		} else {
+			moteIF = new MoteIF(
+					BuildSource.makePhoenix(PrintStreamMessenger.err));
+		}
+	}
+
+	public void start() {
+	}
+
+	private void addMessageType(Message msg) {
+		moteIF.registerListener(msg, this);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Collector reader = new Collector("serial@/dev/ttyUSB0:telosb");
+
+		Message msg = new AppMessage();
+		reader.addMessageType(msg);
+		reader.start();
+	}
+	
+	public void insertdata(int id, double value, String name){
+		sensor sen =new sensor();
+		sensorDAO senDAO = new sensorDAO();
+		sen.setSensorid(id);
+		sen.setName(name);
+		BigDecimal b = new BigDecimal(value); 
+		value = b.setScale(2,BigDecimal.ROUND_HALF_UP).doubleValue();
+		sen.setValue(value);
+		System.out.println("value :"+value);
+		senDAO.insert(sen);
+	}
+
+	public void messageReceived(int i, Message msg) {
+		double co2, temp, humi, light , infra1, infra2;
+		temp = -310;
+		if (msg instanceof AppMessage) {
+			AppMessage appMsg = (AppMessage) msg;
+
+			if (appMsg.get_dataType() == 0) { // CO2
+				co2 = (appMsg.get_data1() / 4096 * 3.0 - 0.8) / 3.2 * 1600 + 400;//ppm
+				int id = appMsg.get_nodeId();
+				insertdata(id,co2,"CO2");
+			} else if (appMsg.get_dataType() == 1) { // temperature
+				// temp = -39.60 + 0.01*val, powered by Batteries
+				// temp = -40.10 + 0.01*val, powered by USB
+				temp = appMsg.get_data1()*0.01-39.6;
+				int id = appMsg.get_nodeId();
+				//写入
+				insertdata(id,temp,"temperature");
+				
+			} else if (appMsg.get_dataType() == 2) { // humidity
+				// Humi = -4 + 0.0405 * val + (-2.8 * 10^-6) * (val^2)
+				// Humi = (Temp - 25) * (0.01 + 0.00008 * valh) + Humi;
+				if(temp < -310){//必须要有温度传感器为前提
+					System.err.println("can't get effective temperature,can not calculate Humidity!");
+				}
+				else{
+					double data = appMsg.get_data1();
+					double value =  -4 + 0.0405 * data + (-2.8 * 10e-6) * (Math.pow(data, 2));
+					value = (temp - 25)*(0.01 + 0.0008 * data)+value;
+				}
+			} else if (appMsg.get_dataType() == 3) { // light
+				// V0 = value / 4096 * 3.0
+				// I = V0 / 100,000
+				// lux = 0.625 * 1e6 * I * 1000, sensing device: S1087
+				// lux = 0.769 * 1e5 * I * 1000, sensing device: S1087-01
+				double lux = appMsg.get_data1()*3.0*769/4096.0;
+				int id = appMsg.get_nodeId();
+				insertdata(id, lux, "luminance");
+			} else if (appMsg.get_dataType() == 4) { // infra
+				double value = appMsg.get_data1();
+				int id = appMsg.get_nodeId();
+				insertdata(id,value,"infrared");
+			}
+			System.out.println("data1:"+appMsg.get_data1() + "	data2:" + appMsg.get_data2() + "    type:" + appMsg.get_dataType()+"    count:"+appMsg.get_count()
+			+ System.getProperty("line.separator"));
+			
+			
+		}
+	}
+}
